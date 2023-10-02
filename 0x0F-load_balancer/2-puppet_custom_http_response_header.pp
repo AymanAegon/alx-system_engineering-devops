0a790e802ba17b42797c7226efe03d138dd50067
AymanAegon <ayo12ammar24@gmail.com>
2023-10-02 13:30:52 -0700

    2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# custom HTTP header configuration with puppet
+
+exec {'header':
+command  => 'hostname=$(cat /etc/hostname) &&
+	     sudo apt-get -y update && 
+             sudo apt-get -y install nginx && 
+             sudo sed -i "40i \\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/sites-enabled/default &&
+             sudo service nginx restart',
+provider => shell,
+}
