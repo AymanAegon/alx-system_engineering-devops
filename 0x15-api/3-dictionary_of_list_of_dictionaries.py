47bcfcbee2a016ef94dc1214c2ca977ea99390cc
ayman <ayo12ammar24@gmail.com>
2023-10-23 13:57:26 -0700

    3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+export data in the json format
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url).json()
+    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    arr = []
+    data = {}
+    for user in users:
+        for todo in response.json():
+            if (todo.get("userId") == user.get("id")):
+                obj = {}
+                obj["username"] = user.get("username")
+                obj["task"] = todo.get("title")
+                obj["completed"] = todo.get("completed")
+                arr.append(obj.copy())
+        data[user.get("id")] = arr
+
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as jsonfile:
+        json.dump(data, jsonfile)
