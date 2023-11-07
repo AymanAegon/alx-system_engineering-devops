a7125ad8305491413202b484389088a3c87691c6
AymanAegon <ayo12ammar24@gmail.comnode>
2023-11-07 16:24:15 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers for a given subreddit """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'YourAppName/1.0'}
+    headers = {'user-agent': 'YourAppName/1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return 0
