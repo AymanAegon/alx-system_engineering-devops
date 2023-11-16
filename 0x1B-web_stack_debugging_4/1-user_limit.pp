2b5010e873fd4fa24d05aab270561e32ff2ce9ad
ayman <ayo12ammar24@gmail.com>
2023-11-16 09:55:29 -0800

    1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,11 @@
 #Increase the file descriptor limit
 
-exec { 'set new limit':
-  command => 'sed -i \'s/^holberton hard nofile .*/holberton hard nofile 90000/g\' /etc/security/limits.conf ; sed -i \'s/^holberton soft nofile .*/holberton soft nofile 90000/g\' /etc/security/limits.conf',
+exec { 'set new hard limit':
+  command => 'sed -i \'s/^holberton hard nofile .*/holberton hard nofile 90000/g\' /etc/security/limits.conf',
   path    => '/bin/',
 }
 
+-> exec { 'set new soft limit':
+  command => 'sed -i \'s/^holberton soft nofile .*/holberton soft nofile 90000/g\' /etc/security/limits.conf',
+  path    => '/bin/',
+}
