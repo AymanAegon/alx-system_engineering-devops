6aa8f3bc59d88b0939892845ff6be57382057b04
ayman <ayo12ammar24@gmail.com>
2023-11-16 08:08:14 -0800

    0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#fix nginx webserver
+
+exec { 'Set new limit':
+  command => 'sed -i \'s/^ULIMIT=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+  path    => '/bin/',
+}
+
+-> exec { 'restart nginx server':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+}
